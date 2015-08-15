abe9c5f51acbcb2b52de51ebccf1574ce54ad4fa
Fransiskus Setiawan <fgn.dwikasetiawan@gmail.com>
2015-08-15 08:20:40 +0700

    Added modal to create new task

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,30 @@
 ///<reference path="typings/tsd.d.ts" />
 
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function($scope, $ionicModal) {
+	$scope.tasks = [];
+	
+	$ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+		$scope.taskModal = modal;
+	}, {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
+	
+	$scope.createTask = function(task) {
+		$scope.tasks.push({
+			title: task.title
+		});
+		$scope.taskModal.hide();
+		task.title = "";
+	};
+	
+	$scope.newTask = function() {
+		$scope.taskModal.show();
+	};
+	
+	$scope.newTask = function() {
+		$scope.taskModal.hide();
+	};
+});
